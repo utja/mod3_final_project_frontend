4159a52d78d3cdea520407469dfed2be8bcdcac0
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-08-21 11:35:22 -0400

    init commit

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMcContentLoaded', () => {
+
+  let button = document.getElementById('button')
+
+  button.addEventListener('click', e => {
+    e.preventDefault()
+  })
+
+})
