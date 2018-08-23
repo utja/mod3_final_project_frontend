bcb9d176d4a4edf9d6b5f92bd16486294dcc31f5
Rose <rose1264@gmail.com>
2018-08-23 15:33:56 -0400

    gravity_version

--- index.js
+++ index.js
@@ -27,7 +27,6 @@ document.addEventListener('DOMContentLoaded', function() {
     },
     clear : function() {
       this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      // console.log('clear')
     }
 
   }
@@ -43,46 +42,16 @@ document.addEventListener('DOMContentLoaded', function() {
     this.width = width
     this.height = height
     this.color = color
-    // this.speedX = 0
-    // this.speedY = 0
     this.x = x
     this.y = y
     this.gravity = 0
     this.gravitySpeed = 0
   }
 
-
-  // Obstacle.prototype.animate = function(){
-  //   let ctx = gameArea.context
-  //
-  //   setInterval(function(){
-  //     // let ctx = gameArea.context
-  //     ctx.clearRect(this.x+90, this.y, 10, this.height)
-  //     this.x -= 10
-  //
-  //     // ctx.fillStyle = this.color
-  //     // ctx.fillRect(this.x, this.y, this.width, this.height)
-  //     this.render()
-  //     // console.log(this.x)
-  //   }.bind(this), 100)
-  //
-  // }
-
-
-  // function render(object) {
-  //   let ctx = gameArea.context
-  //   ctx.clearRect(0,0, gameArea.canvas.width, gameArea.canvas.height)
-  //
-  //   ctx.fillStyle = object.color
-  //   ctx.fillRect(object.x, object.y, object.width, object.height)
-  // }
-
   Obstacle.prototype.renderObs = function(){
     let ctx = gameArea.context
-    // ctx.clearRect(this.x, this.y, this.width, this.height)
     ctx.clearRect(0, 0, gameArea.canvas.width, gameArea.canvas.height)
     this.x -= 3
-    // ctx.beginPath()
     obstArray.forEach(function(obs){
       ctx.fillStyle = obs.color
       ctx.fillRect(obs.x, obs.y, obs.width, obs.height)
@@ -93,26 +62,9 @@ document.addEventListener('DOMContentLoaded', function() {
     let ctx = gameArea.context
 
     this.x -= 10
-
-    // render()
   }
 
   let obstArray = []
-
-
-  // function to generate obstacles
-  // const generateObstacles = setInterval(function(){
-  //   const y = Math.floor(Math.random()*gameArea.canvas.height)
-  //   const x = gameArea.canvas.width
-  //   const width = 50
-  //   const height = Math.floor(Math.random()*500)
-  // //
-  //   const newObs = new Obstacle(x, y, width, height, 'green')
-  //   // obstArray.push(newObs)
-  //   // newObs.renderObs()
-  //   newObs.moveObs()
-  //   // console.log('generate')
-  // }, 1000)
 
   const generateObstacle = function() {
     if (gameEnded === false) {
@@ -123,48 +75,34 @@ document.addEventListener('DOMContentLoaded', function() {
 
       const newObs = new Obstacle(x, y, width, height, 'green')
       obstArray.push(newObs)
-  }
-    // newObs.renderObs()
+    }
   }
 
   const animateObstacles = function() {
     obstArray.forEach(obstacle => {
       obstacle.renderObs()
-      // obstacle.moveObs()
-
-      // console.log(obstacle.x)
     })
   }
 
+  let ctx = gameArea.context
 
+  const hero = new Image();
+  hero.src = 'imgs/hero.png';
+  hero.width = 50
+  hero.height = 50
+  hero.speedX = 0
+  hero.speedY = 0
+  hero.posX = 0
+  hero.posY = 0
+  hero.gravity = 0.05
+  hero.gravitySpeed = 0
 
-  // console.log(newObs)
-  // let obs = new Obstacle(350, 0, 50, 200, 'green')
-  //
-  // let obsTwo = new Obstacle(350, 500, 50, 200, 'green')
-
-  // render(obs)
-  // obs.animate()
-
-  const Hero = function(x, y, width, height, color) {
-    this.score = 0
-    this.width = width
-    this.height = height
-    this.color = color
-    // this.speedX = 0
-    // this.speedY = 0
-    this.x = x
-    this.y = y
-    this.gravity = 0
-    this.gravitySpeed = 0
-  }
-
-  Hero.prototype.checkCollision = function (obstacle) {
+  hero.checkCollision = function(obstacle) {
     //hero
-    this.left = this.x
-    this.right = this.x + this.width
-    this.top = this.y
-    this.bottom = this.y + this.height
+    this.left = this.posX
+    this.right = this.posX + this.width
+    this.top = this.posY
+    this.bottom = this.posY + this.height
 
     //obstacle
     obstacle.left = obstacle.x
@@ -178,40 +116,28 @@ document.addEventListener('DOMContentLoaded', function() {
       gameArea.clear()
       obstArray = []
       gameEnded = true
-      hero.x = 0
-      hero.y = 300
+      hero.posX = 0
+      hero.posY = 0
 
       gameArea.start()
     }
   }
 
-  const hero = new Hero(0, 0, 75, 75, "blue",)
 
   function render() {
-    let ctx = gameArea.context
-    // ctx.clearRect(hero.x, hero.y, hero.width, hero.height)
-    // ctx.clearRect(0, 0, gameArea.canvas.width, gameArea.canvas.height)
-    //
-    // ctx.fillStyle = obs.color
-    // ctx.fillRect(obs.x, obs.y, obs.width, obs.height)
-
-    // ctx.fillStyle = obsTwo.color
-    // ctx.fillRect(obsTwo.x, obsTwo.y, obsTwo.width, obsTwo.height)
-
-    ctx.fillStyle = hero.color
-    ctx.fillRect(hero.x, hero.y, hero.width, hero.height)
-
+    hero.gravitySpeed += hero.gravity;
+    hero.posY += hero.speedY + hero.gravitySpeed
+    ctx.drawImage(hero, hero.posX, hero.posY)
     // debugger
-    // hero.checkCollision(obsTwo)
-    // hero.checkCollision(obs)
   }
 
-  // render()
+  function accelerate(n) {
+      hero.gravity = n;
+  }
 
   const play = function() {
-  setInterval(generateObstacle, 1500)
+  setInterval(generateObstacle, 2500)
   setInterval(() => {
-    // generateObstacle()
     if (gameEnded === false) {
       animateObstacles()
       render()
@@ -222,62 +148,15 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   play()
-  //keydown event listener for hero
-  document.onkeydown = function(e) {
-    let ctx = gameArea.context
-    let moveAmount = 100
-    // ctx.clearRect(hero.x, hero.y, hero.width, hero.height)
-    const oldX = hero.x
-    const oldY = hero.y
-    switch (e.key) {
-      case 'ArrowUp':
-        e.preventDefault()
-        hero.y -= moveAmount
-        // ctx.clearRect(oldX, oldY, hero.width, hero.height)
-        if (hero.y < 0) {
-          hero.y = 0
-        }
-        break
-      case 'ArrowDown':
-        e.preventDefault()
-        hero.y += moveAmount
-        // ctx.clearRect(oldX, oldY, hero.width, hero.height)
-        if (hero.y > gameArea.canvas.height - hero.height) {
-          hero.y = gameArea.canvas.height - hero.height
-        }
-        break
-      case 'ArrowLeft':
-        e.preventDefault()
-        hero.x -= moveAmount
-        // ctx.clearRect(oldX, oldY, hero.width, hero.height)
-        if (hero.x < 0) {
-          hero.x = 0
-        }
-        break
-      case 'ArrowRight':
-        e.preventDefault()
-        hero.x += moveAmount
-        // ctx.clearRect(oldX, oldY, hero.width, hero.height)
-        if (hero.x > gameArea.canvas.width - hero.width) {
-          hero.x = gameArea.canvas.width - hero.width
-        }
-        break
-      default:
-        break
-
-    }
+  let accelerateBtn = document.getElementById('accelerate')
+  accelerateBtn.onmousedown = function() {
+    hero.gravity = -0.2
+    render()
+  }
+  accelerateBtn.onmouseup = function() {
+    hero.gravity = 0.05
+    render()
   }
 
-
-
-
-  // let keyCheck = document.addEventListener('keydown', e => {
-  //   e.preventDefault()
-  //
-  //   })
-
-  // obs.render.call(hero)
-  // console.log('heasdllo')
   // debugger
-
 })
